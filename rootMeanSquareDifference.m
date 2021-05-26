a380852f216925ea3626938b107525e3273a0b09
stanzima <stanzima@gmail.com>
2021-05-26 12:27:21 +0200

    Update rootMeanSquareDifference.m

--- rootMeanSquareDifference.m
+++ rootMeanSquareDifference.m
@@ -1,19 +1,17 @@
+function RMSD = rootMeanSquareDifference(ref_test, ref_est)
 %   This function calculates root mean square difference(rmsd) between the test spectra and the estimated spectra and
 %   returns rmsd.
 %
 %   Example:
-%   RMSD = rootMeanSquareDifference(R_test, R_est)
-%
+%   RMSD = rootMeanSquareDifference(ref_test, ref_est)
 %   
 %   author:          © Tanzima Habib
 %   version:         1.1
 %   date:  	         1-05-2021
 %########################################################################################################################################################
-%ref_test = test reflectance, ref_est= estimated reflectance,
+% ref_test = test reflectance, ref_est= estimated reflectance,
 %########################################################################################################################################################
 
-function RMSD = rootMeanSquareDifference(R_test, R_est)
-
-    RMSD= mean(sqrt(mean((R_test - R_est).^2,2)));
+    RMSD= mean(sqrt(mean((ref_test - ref_est).^2,2)));
 
 end
